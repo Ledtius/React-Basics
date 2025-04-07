4a1592e9f5896b6f37ec697c6b2e498c5db0b4a3
Calet Devia <calebtius@gmail.com>
2025-04-07 13:02:09 -0500

    Created the order of the todo-list files

---
+++ src/todo-list-react/components/form/form.jsx
@@ -0,0 +1,20 @@
+// import "../form/form.module.css";
+import formStyles from "./form.module.css";
+
+console.log(formStyles);
+
+export function FormTodoList() {
+  return (
+    <form className={formStyles["todo-list__bar"]}>
+      <input
+        className={formStyles["todo-list__bar-input"]}
+        type="text"
+        placeholder="Digita tu tarea del dia"
+      />
+
+      <button className={formStyles["todo-list__bar-button"]} type="button">
+        Añadir
+      </button>
+    </form>
+  );
+}
