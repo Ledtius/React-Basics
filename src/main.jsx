14ec7dc2e9c8510ef6f84a0786e5a1e31bd2b82f
Calet Devia <calebtius@gmail.com>
2025-04-09 15:53:43 -0500

    Added the div to the items and the class too

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React, { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 
 import { FormTodoList } from "./todo-list-react/components/form/form.jsx";
+
+import { ItemList } from "./todo-list-react/components/item/item.jsx";
 
 import "./general.css";
 
@@ -10,6 +13,9 @@ const root = createRoot(document.getElementById("root")).render(
     <div className="todo-list">
       <h1 className="todo-list__title">Lista de tareas</h1>
       <FormTodoList />
+    </div>
+    <div className="todo-list__content">
+      <ItemList />
     </div>
   </>
 );
