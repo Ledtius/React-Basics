1716437ab77eb385d4a1302fe0e2531dd3f44c58
Calet Devia <calebtius@gmail.com>
2025-04-12 15:33:17 -0500

    Added stage to the input

--- src/todo-list-react/components/form/form.jsx
+++ src/todo-list-react/components/form/form.jsx
@@ -1,15 +1,25 @@
 // import "../form/form.module.css";
+import { useState } from "react";
 import formStyles from "./form.module.css";
 
 console.log(formStyles);
 
 export function FormTodoList() {
+  const [text, setText] = useState("");
+
+  const handleInputChange = (e) => {
+    setText(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
     <form className={formStyles["todo-list__bar"]}>
       <input
         className={formStyles["todo-list__bar-input"]}
         type="text"
         placeholder="Digita tu tarea del dia"
+        onInput={handleInputChange}
+        value={text}
       />
       <button className={formStyles["todo-list__bar-button"]} type="button">
         Añadir
