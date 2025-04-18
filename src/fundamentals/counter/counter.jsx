02ad37fdca7ec916d0cc77d0e12f4a00ff107718
Calet Devia <calebtius@gmail.com>
2025-04-18 07:34:27 -0500

    Added the functions to increase and decrease the counter

--- src/fundamentals/counter/counter.jsx
+++ src/fundamentals/counter/counter.jsx
@@ -1,12 +1,29 @@
 import counterStyle from "./counter.module.css";
 
+import { useState } from "react";
+
 export const CounterComponent = () => {
+  let [counter, setCounter] = useState(0);
+  const plusFunction = () => {
+    setCounter(counter + 1);
+  };
+  const minusFunction = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <div className={counterStyle["counter"]}>
-      <p className={counterStyle["counter__value"]}>Valor</p>
+      <p className={counterStyle["counter__value"]}>{counter}</p>
       <div className={counterStyle["counter__btns"]}>
-        <button className={counterStyle["counter__btn"]}>+</button>
-        <button className={counterStyle["counter__btn"]}>-</button>
+        <button className={counterStyle["counter__btn"]} onClick={plusFunction}>
+          +
+        </button>
+        <button
+          className={counterStyle["counter__btn"]}
+          onClick={minusFunction}
+        >
+          -
+        </button>
       </div>
     </div>
   );
