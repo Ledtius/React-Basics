77d6ca804f734ad62322fc9864c951d4b1212151
Calet Devia <calebtius@gmail.com>
2025-04-18 05:54:29 -0500

    Added classes in the tags

--- src/fundamentals/counter/counter.jsx
+++ src/fundamentals/counter/counter.jsx
@@ -2,11 +2,11 @@ import counterStyle from "./counter.module.css";
 
 export const CounterComponent = () => {
   return (
-    <div className={counterStyle["count"]}>
-      <p>Valor</p>
-      <div>
-        <button>+</button>
-        <button>-</button>
+    <div className={counterStyle["counter"]}>
+      <p className={counterStyle["counter__value"]}>Valor</p>
+      <div className={counterStyle["counter__btns"]}>
+        <button className={counterStyle["counter__btn"]}>+</button>
+        <button className={counterStyle["counter__btn"]}>-</button>
       </div>
     </div>
   );
