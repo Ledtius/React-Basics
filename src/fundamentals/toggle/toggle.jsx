e17335cc59ee0eeb9b89c05b66e5db052d6ba80f
Calet Devia <calebtius@gmail.com>
2025-04-22 16:00:28 -0500

    Added a p tag and the variable name of the css class

--- src/fundamentals/toggle/toggle.jsx
+++ src/fundamentals/toggle/toggle.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
 import toggleStyles from "./toggle.module.css";
+import { CheckCheck } from "lucide-react";
 
 export const ToggleComponent = () => {
+  const [check, setCheck] = useState(false);
+  const [styleText, setStyleText] = useState("accordion__text");
+
+  const checkFunction = (e) => {
+    const checkValue = e.target.checked;
+
+    if (checkValue === false) {
+      setStyleText("accordion__text--inactive");
+    } else {
+      setStyleText("accordion__text");
+    }
+
+    console.log(checkValue);
+  };
+
   return (
     <>
       <div className={toggleStyles["accordion"]}>
@@ -8,13 +26,15 @@ export const ToggleComponent = () => {
           className={toggleStyles["accordion__checkbox"]}
           type="checkbox"
           id="check"
-          
+          onChange={checkFunction}
         />
-        <label className={toggleStyles["accordion__text"]} htmlFor="check">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fuga
-          inventore eius magnam hic blanditiis, nam provident aperiam
-          consequuntur laboriosam perferendis repudiandae, accusantium quaerat
-          officia, maxime est ipsum! Possimus, sapiente?
+        <label className={toggleStyles["accordion__label"]} htmlFor="check">
+          <p className={toggleStyles[styleText]}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fuga
+            inventore eius magnam hic blanditiis, nam provident aperiam
+            consequuntur laboriosam perferendis repudiandae, accusantium quaerat
+            officia, maxime est ipsum! Possimus, sapiente?
+          </p>
         </label>
       </div>
     </>
