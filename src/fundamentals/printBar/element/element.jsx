d453e0d60b6d465c523a0c382d2445752e12b240
Calet Devia <calebtius@gmail.com>
2025-04-21 19:44:11 -0500

    Added props to the element

--- src/fundamentals/printBar/element/element.jsx
+++ src/fundamentals/printBar/element/element.jsx
@@ -1,10 +1,10 @@
 import elementStyle from "./element.module.css";
 
-export const ElementComponent = () => {
+export const ElementComponent = ({ name }) => {
   return (
     <>
       <div className={elementStyle["element"]}>
-        <span className={elementStyle["element__text"]}>sdfs</span>
+        <span className={elementStyle["element__text"]}>{name}</span>
       </div>
     </>
   );
