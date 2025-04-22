580df7d19bbc30c4ec47e0658fd7ab33beb8b54d
Calet Devia <calebtius@gmail.com>
2025-04-21 19:42:38 -0500

    Imported and used the elementComponent

--- src/fundamentals/printBar/bar/bar.jsx
+++ src/fundamentals/printBar/bar/bar.jsx
@@ -1,11 +1,14 @@
 import barStyles from "./bar.module.css";
 
+import { ElementComponent } from "../element/element";
+
 import { useState } from "react";
 
 import { Send } from "lucide-react";
 
 export const BarComponent = () => {
   let [input, setInput] = useState("");
+  let name = ".";
 
   let handleInputChange = (e) => {
     setInput(e.target.value.trim());
@@ -14,28 +17,37 @@ export const BarComponent = () => {
 
   let handleSendMessage = () => {
     // input = setInput;
+
+    name = input;
+    console.log(name);
     console.log(`Texto: ${input}`);
+    return input;
   };
 
   return (
     <>
-      <div className={barStyles["form"]}>
-        <h1 className={barStyles["form__title"]}>Cambia el valor del elemento</h1>
-        <div className={barStyles["form__bar"]}>
-          <input
-            className={barStyles["form__input"]}
-            type="text"
-            placeholder="Escribe algo..."
-            value={input}
-            onInput={handleInputChange}
-          />
-          <button className={barStyles["form__btn"]} type="button">
-            <Send
-              className={barStyles["form__icon"]}
-              onClick={handleSendMessage}
+      <div className="container">
+        <div className={barStyles["form"]}>
+          <h1 className={barStyles["form__title"]}>
+            Cambia el valor del elemento
+          </h1>
+          <div className={barStyles["form__bar"]}>
+            <input
+              className={barStyles["form__input"]}
+              type="text"
+              placeholder="Escribe algo..."
+              value={input}
+              onInput={handleInputChange}
             />
-          </button>
+            {/* <button className={barStyles["form__btn"]} type="button">
+              <Send
+                className={barStyles["form__icon"]}
+                onClick={handleSendMessage}
+              />
+            </button> */}
+          </div>
         </div>
+        <ElementComponent name={input} />
       </div>
     </>
   );
