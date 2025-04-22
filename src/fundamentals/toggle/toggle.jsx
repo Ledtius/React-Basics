7d0388c471d76764abb66d75608e1e785ff2c108
Calet Devia <calebtius@gmail.com>
2025-04-21 19:53:34 -0500

    Create the toggle files

---
+++ src/fundamentals/toggle/toggle.jsx
@@ -0,0 +1,13 @@
+import toggleStyles from "./toggle.module.css";
+
+export const ToggleComponent = () => {
+  <>
+    <div>
+     <div>
+     <input type="checkbox" name="" id="" />
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fuga inventore eius magnam hic blanditiis, nam provident aperiam consequuntur laboriosam perferendis repudiandae, accusantium quaerat officia, maxime est ipsum! Possimus, sapiente?</p>
+     </div>
+
+    </div>
+  </>;
+};
