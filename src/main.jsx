102df8214d640bbb48028ae838dc509c17702759
Calet Devia <calebtius@gmail.com>
2025-04-24 04:59:26 -0500

    Imported the form and the item component

--- src/main.jsx
+++ src/main.jsx
@@ -2,55 +2,18 @@ import React, { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 
-import { TableComponent } from "./fundamentals/table/table";
-
-import tableStyles from "../src/fundamentals/table/table.module.css";
-
 import "./general.css";
 
-const students = [
-  { name: "Juan", age: 16 },
-  { name: "Maria", age: 22 },
-  { name: "Carlos", age: 17 },
-  { name: "Luisa", age: 21 },
-];
+import { FormComponent } from "./todo-list-react/components/form/form";
+
+import { ItemComponent } from "./todo-list-react/components/item/item";
 
 const root = createRoot(document.getElementById("root")).render(
   <>
-    <div className="container">
-      <table className={tableStyles["table"]}>
-        <thead className={tableStyles["table__head"]}>
-          <tr className={tableStyles["table__row"]}>
-            <th className={tableStyles["table__column"]}>Name</th>
-            <th className={tableStyles["table__column"]}>Age</th>
-            <th className={tableStyles["table__column"]}>Under-age/Adult</th>
-          </tr>
-        </thead>
-        <tbody className={tableStyles["table__body"]}>
-          {students.map(({ name, age }, index) => (
-            <TableComponent name={name} age={age} key={index} />
-          ))}
-        </tbody>
-      </table>
-    </div>
-    {/* Second exercise */}
-    {/* <CounterComponent /> */}
-    {/* First exercise */}
-
-    {/* <GreetingsComponent greeting="Ejercicio numero 1 cumplido" /> */}
-
-    {/* Todo-list */}
-
-    {/* 
-  
     <div className="todo-list">
       <h1 className="todo-list__title">Lista de tareas</h1>
-      <FormTodoList />
+      <FormComponent />
+      <ItemComponent />
     </div>
-    <div className="todo-list__content">
-      <ItemList taskName="Tarea componetizada" id="1" />
-      <ItemList taskName="Tarea componetizadasdsddd 23" id="2" />
-    </div>
-      */}
   </>
 );
