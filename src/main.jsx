54390b7a6d2da4a303a0d2832495697926069dda
Calet Devia <calebtius@gmail.com>
2025-04-28 17:21:37 -0500

    Added the component list of the task

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,14 @@ import { FormComponent } from "./todo-list-react/components/form/form";
 
 import { ItemComponent } from "./todo-list-react/components/item/item";
 
+import { ListComponent } from "./todo-list-react/components/list/list.jsx";
+
 const root = createRoot(document.getElementById("root")).render(
   <>
     <div className="todo-list">
       <h1 className="todo-list__title">Lista de tareas</h1>
       <FormComponent />
+      <ListComponent />
     </div>
   </>
 );
