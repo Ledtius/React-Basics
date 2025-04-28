2a9c2e3c6611ae2da665cf8a5c090279f7aa087b
Calet Devia <calebtius@gmail.com>
2025-04-28 17:22:58 -0500

    Deleted the item component and the class list of the task list

--- src/todo-list-react/components/form/form.jsx
+++ src/todo-list-react/components/form/form.jsx
@@ -1,42 +1,12 @@
 // import "../form/form.module.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import formStyles from "./form.module.css";
 
 import { ItemComponent } from "../item/item";
-import { Component } from "lucide-react";
 
 export function FormComponent() {
-  const [task, setTask] = useState("");
-  const [taskList, setTaskList] = useState([]);
-
-  const handleTask = (e) => {
-    // console.log(task);
-    const inputValue = e.target.value;
-
-    setTask(inputValue);
-
-
-    console.log(e.currentTarget.textContent);
-    // console.log(task);
-  };
-
-  const handleList = (e) => {
-    e.preventDefault();
-    setTaskList([...taskList, task]);
-    const inputElement = e.target.classList.contains("todo-list__bar-input");
-    // useEffect(() => {});
-    console.log(inputElement);
-   setTask("");
-
-  };
-
-  const handleDelete = (id) => {
-    console.log(id);
-    console.log(taskList);
-    setTaskList(taskList.filter((_, index) => id !== index));
-    console.log(taskList);
-  };
+  const [task, setTask] = useState({ name: "", id: "", state: false });
 
   return (
     <div className="todo-list">
@@ -45,27 +15,11 @@ export function FormComponent() {
           className={formStyles["todo-list__bar-input"]}
           type="text"
           placeholder="Digita tu tarea del dia"
-          value={task}
-          onInput={handleTask}
         />
-        <button
-          className={formStyles["todo-list__bar-button"]}
-          type="button"
-          onClick={handleList}
-        >
+        <button className={formStyles["todo-list__bar-button"]} type="button">
           Añadir
         </button>
       </form>
-      <div className={formStyles["todo-list__tasks"]}>
-        {taskList.map((task, index) => (
-          <ItemComponent
-            key={index}
-            id={index}
-            taskName={task}
-            deleteItem={() => handleDelete(index)}
-          />
-        ))}
-      </div>
     </div>
   );
 }
