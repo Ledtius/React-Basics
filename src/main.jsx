e26c925bc4db6076d3b8d061c2534e88ace04860
Calet Devia <calebtius@gmail.com>
2025-04-29 10:15:16 -0500

    Added the function app and the task list in there

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,29 @@
 import React, { StrictMode } from "react";
 
-import { createRoot } from "react-dom/client";
+import { useState } from "react";
 
-import "./general.css";
+import { createRoot } from "react-dom/client";
 
 import { FormComponent } from "./todo-list-react/components/form/form";
 
-import { ItemComponent } from "./todo-list-react/components/item/item";
-
 import { ListComponent } from "./todo-list-react/components/list/list.jsx";
 
-const root = createRoot(document.getElementById("root")).render(
-  <>
+import "./general.css";
+
+function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  return (
     <div className="todo-list">
       <h1 className="todo-list__title">Lista de tareas</h1>
-      <FormComponent />
-      <ListComponent />
+      <FormComponent taskList={taskList} setTaskList={setTaskList} />
+      <ListComponent taskList={taskList} />
     </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <App />
   </>
 );
