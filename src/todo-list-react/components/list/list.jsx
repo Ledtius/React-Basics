174a74cc57c0f8a388573ed105e088093feaf2fe
Calet Devia <calebtius@gmail.com>
2025-04-29 14:11:30 -0500

    Deleted the other props

--- src/todo-list-react/components/list/list.jsx
+++ src/todo-list-react/components/list/list.jsx
@@ -1,11 +1,17 @@
 import listStyles from "./list.module.css";
 import { ItemComponent } from "../item/item";
 
-export const ListComponent = ({ taskList }) => {
+export const ListComponent = ({ taskList, setTaskList }) => {
   return (
     <div className={listStyles["todo-list__tasks"]}>
       {taskList.map((task) => (
-        <ItemComponent taskName={task.name} id={task.id} key={task.id} />
+        <ItemComponent
+          taskName={task.name}
+          id={task.id}
+          key={task.id}
+          taskList={taskList}
+          setTaskList={setTaskList}
+        />
       ))}
     </div>
   );
