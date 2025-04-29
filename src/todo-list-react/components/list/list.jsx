182d914b32748fcc05b1f8d5186091ba30cf7710
Calet Devia <calebtius@gmail.com>
2025-04-29 10:20:56 -0500

    Added the map and the corrects props of the item

--- src/todo-list-react/components/list/list.jsx
+++ src/todo-list-react/components/list/list.jsx
@@ -1,10 +1,12 @@
 import listStyles from "./list.module.css";
 import { ItemComponent } from "../item/item";
 
-export const ListComponent = () => {
+export const ListComponent = ({ taskList }) => {
   return (
     <div className={listStyles["todo-list__tasks"]}>
-      <ItemComponent />
+      {taskList.map((task) => (
+        <ItemComponent taskName={task.name} id={task.id} key={task.id} />
+      ))}
     </div>
   );
 };
