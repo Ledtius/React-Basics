9819e97b41897c8f678a39d6f49e62078e902886
Calet Devia <calebtius@gmail.com>
2025-05-01 07:05:08 -0500

    Used setTimeOut

--- src/main.jsx
+++ src/main.jsx
@@ -13,54 +13,20 @@ import "./general.css";
 function ProveThings() {
   const [count, setCount] = useState(0);
 
-  const [count2, setCount2] = useState(0);
-
-  // const [totalC, setTotalC] = useState(0);
-
   const addCount = () => {
-    setCount(count + 1);
-  };
-  const addCount2 = () => {
-    setCount2(count2 + 1);
-  };
-
-  const subCount = () => {
-    setCount(count - 1);
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 2000);
   };
 
-  const subCount2 = () => {
-    setCount2(count2 - 1);
-  };
-
-  const resetCount = () => {
-    setCount(0);
-  };
-  const resetCount2 = () => {
-    setCount2(0);
-  };
   return (
     <>
       <div>
         <h3>Count 1</h3>
         <div>
           <button onClick={addCount}>+</button>
-          <button onClick={subCount}>-</button>
-          <button onClick={resetCount}>R</button>
         </div>
         <span>{count}</span>
-      </div>
-      <div>
-        <h3>Count 2</h3>
-        <div>
-          <button onClick={addCount2}>+</button>
-          <button onClick={subCount2}>-</button>
-          <button onClick={resetCount2}>R</button>
-        </div>
-        <span>{count2}</span>
-      </div>
-      <div>
-        <h3>Mix Count Values </h3>
-        <span>{count + count2}</span>
       </div>
     </>
   );
