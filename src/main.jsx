10f2710a95e60d12e138254b57b1a95d12265828
Calet Devia <calebtius@gmail.com>
2025-05-06 09:39:31 -0500

    Finished the form exercise

--- src/main.jsx
+++ src/main.jsx
@@ -13,10 +13,93 @@ import "./general.css";
 import { ToggleLeft } from "lucide-react";
 
 function ProveThings() {
-  // console.log(toggle);
+  /* **Formulario con múltiples campos**: Usa un solo estado para manejar los valores de un formulario (`nombre`, `email`, `edad`) y actualízalos de forma controlada. */
+
+  const [formData, setFormData] = useState({
+    name: "none name",
+    lastName: "none last name",
+    email: "none@gmail.com",
+    age: 0,
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    console.log(name, value);
+
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleForm = (e) => {
+    e.preventDefault();
+  };
+
+  const handleFormSubmit = () => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      name: "",
+      lastName: "",
+      email: "",
+      age: 0,
+    }));
+  };
+
+  console.log(formData);
+  useEffect(() => {
+    console.log(formData.name);
+    console.log(formData.lastName);
+    console.log(formData.email);
+    console.log(formData.age);
+  }, [formData]);
   return (
     <>
-      <div></div>
+      <form onSubmit={handleForm}>
+        <div>
+          <label htmlFor="fName">First Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            id="fName"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="lName">Last Name:</label>
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            id="lName"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            name="age"
+            value={formData.age}
+            id="age"
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit" onClick={handleFormSubmit}>
+          Send
+        </button>
+      </form>
     </>
   );
 }
