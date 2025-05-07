3f8316dbf5f5cd5a0d1f3798bff8e988c99ffed7
Calet Devia <calebtius@gmail.com>
2025-05-07 14:57:50 -0500

    Added TaskApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React, { StrictMode, use } from "react";
 
-import { useState, useEffect } from "react";
-
 import { createRoot } from "react-dom/client";
 
 import "./general.css";
+
+import TaskApp from "./projects/task-management/TaskApp";
 
 function App() {
   return <h1>hola</h1>;
@@ -12,6 +12,7 @@ function App() {
 
 createRoot(document.getElementById("root")).render(
   <>
+    <TaskApp />
     <App />
   </>
 );
