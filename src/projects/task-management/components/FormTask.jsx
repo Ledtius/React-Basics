554580e2a69d262088430b4939ce40daea7e5b3f
Calet Devia <calebtius@gmail.com>
2025-05-08 11:44:51 -0500

    Changed its class names

--- src/projects/task-management/components/FormTask.jsx
+++ src/projects/task-management/components/FormTask.jsx
@@ -1,15 +1,27 @@
+import FormTaskStyles from "../styles/FormTask.module.css";
+
 const FormTask = () => {
   const handleForm = (e) => {
     e.preventDefault();
   };
   return (
     <>
-      <form onSubmit={handleForm}>
-        <div>
-          <label htmlFor="bar">Lista de tareas</label>
-          <input type="text" id="bar" />
+      <form className={FormTaskStyles["task-app__form"]}>
+        <div className={FormTaskStyles["task-app__header"]}>
+          <label className={FormTaskStyles["task-app__title"]} htmlFor="bar">
+            Lista de tareas
+          </label>
+          <div className={FormTaskStyles["task-app__input-group"]}>
+            <input
+              className={FormTaskStyles["task-app__input"]}
+              type="text"
+              id="bar"
+            />
+            <button className={FormTaskStyles["task-app__button"]}>
+              Enviar
+            </button>
+          </div>
         </div>
-        <button>Enviar</button>
       </form>
     </>
   );
