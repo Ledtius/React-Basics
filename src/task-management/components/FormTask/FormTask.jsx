40e4aa522bc2117543d4950177d7cdbf9f5fb23e
Calet Devia <calebtius@gmail.com>
2025-05-13 12:44:08 -0500

    I added the functions to handle the form and the send button then I defined the value of the task name with the input and pushed the task in the arrayTask

--- src/task-management/components/FormTask/FormTask.jsx
+++ src/task-management/components/FormTask/FormTask.jsx
@@ -1,19 +1,45 @@
+import { useEffect } from "react";
+
 import FormStyles from "../FormTask/FormTask.module.css";
 
 import { LucideSend } from "lucide-react";
 
-const FormTask = () => {
+const FormTask = ({ task, setTask, taskList, setTaskList }) => {
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    setTask({ ...task, name: "" });
+
+    setTaskList([...taskList, task]);
+  };
+
+  const handleInput = (e) => {
+    const inputValue = e.target.value;
+    console.log(inputValue);
+
+    setTask({ ...task, name: inputValue });
+  };
+
+  useEffect(() => {
+    console.log(task);
+  }, [task]);
+
+  useEffect(() => {
+    console.log(taskList);
+  }, [taskList]);
+
   return (
     <>
-      <form className={FormStyles.form}>
+      <form onSubmit={handleForm} className={FormStyles.form}>
         <h1 className={FormStyles.title}>Gestor de tareas</h1>
         <div className={FormStyles.inputBtn}>
           <input
             className={FormStyles.input}
             type="text"
-            name=""
+            name="name"
+            value={task.name}
             placeholder="Digita tu tarea aqui"
-            id=""
+            onChange={handleInput}
           />
           <button className={FormStyles.btn}>
             <LucideSend className={FormStyles.icon} />
