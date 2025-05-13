715ada225b76ba023aebf8c0119bf47908048c1c
Calet Devia <calebtius@gmail.com>
2025-05-13 07:35:13 -0500

    Deleted old imports etc

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React, { StrictMode, use } from "react";
 
 import { createRoot } from "react-dom/client";
 
-import "./general.css";
+import "./task-management/styles/general.css";
 
-import TaskApp from "./projects/task-management/TaskApp";
+import "./task-management/styles/variables.css";
+
+import TaskApp from "./task-management/pages/TaskApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
