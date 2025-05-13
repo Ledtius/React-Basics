f1ef10463d919d9b55678cf7ca2afca63071da32
Calet Devia <calebtius@gmail.com>
2025-05-13 12:41:54 -0500

    Added the useState variables like: task, and taskList also I exported to the FormTask component

--- src/task-management/pages/TaskApp.jsx
+++ src/task-management/pages/TaskApp.jsx
@@ -1,9 +1,24 @@
 import FormTask from "../components/FormTask/FormTask.jsx";
 
+import { use, useEffect, useState } from "react";
+
 const TaskApp = () => {
+  const [task, setTask] = useState({ name: "", description: "", state: false });
+
+  const [taskList, setTaskList] = useState([]);
+
+  useEffect(() => {
+    console.log(task);
+  }, [task]);
+
   return (
     <>
-      <FormTask />
+      <FormTask
+        task={task}
+        setTask={setTask}
+        taskList={taskList}
+        setTaskList={setTaskList}
+      />
     </>
   );
 };
