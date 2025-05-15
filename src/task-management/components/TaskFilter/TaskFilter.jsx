23229f49c7f0a855aa3c115121164ceac0407a3c
Calet Devia <calebtius@gmail.com>
2025-05-15 12:22:44 -0500

    Added the parent container of the component

--- src/task-management/components/TaskFilter/TaskFilter.jsx
+++ src/task-management/components/TaskFilter/TaskFilter.jsx
@@ -5,30 +5,32 @@ import { ListTodo, LayoutList, ListChecks, Search } from "lucide-react";
 const TaskFilter = () => {
   return (
     <>
-      <div className={FilterStyles.filter}>
-        <button className={FilterStyles.btn}>
-          <ListTodo className={FilterStyles.icon} />
-          <span className={FilterStyles.text}>Todas</span>
-        </button>
-        <button className={FilterStyles.btn}>
-          <LayoutList className={FilterStyles.icon} />
-          <span className={FilterStyles.text}>Incompletas</span>
-        </button>
-        <button className={FilterStyles.btn}>
-          <ListChecks className={FilterStyles.icon} />
-          <span className={FilterStyles.text}>Completas</span>
-        </button>
+      <div className={FilterStyles.filterForm}>
+        <div className={FilterStyles.filter}>
+          <button className={FilterStyles.btn}>
+            <ListTodo className={FilterStyles.icon} />
+            <span className={FilterStyles.text}>Todas</span>
+          </button>
+          <button className={FilterStyles.btn}>
+            <LayoutList className={FilterStyles.icon} />
+            <span className={FilterStyles.text}>Incompletas</span>
+          </button>
+          <button className={FilterStyles.btn}>
+            <ListChecks className={FilterStyles.icon} />
+            <span className={FilterStyles.text}>Completas</span>
+          </button>
+        </div>
+        <form className={FilterStyles.form}>
+          <input
+            className={FilterStyles.input}
+            type="text"
+            placeholder="Busca tu tarea aqui"
+          />
+          <button className={FilterStyles.formBtn}>
+            <Search className={FilterStyles.formIcon} />
+          </button>
+        </form>
       </div>
-      <form className={FilterStyles.form}>
-        <input
-          className={FilterStyles.input}
-          type="text"
-          placeholder="Busca tu tarea aqui"
-        />
-        <button className={FilterStyles.formBtn}>
-          <Search className={FilterStyles.formIcon} />
-        </button>
-      </form>
     </>
   );
 };
