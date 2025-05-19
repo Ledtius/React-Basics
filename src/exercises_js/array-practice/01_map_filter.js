9d0ba73b69d67c7e1dc3521bfa82541c67b8329c
Calet Devia <calebtius@gmail.com>
2025-05-19 14:58:54 -0500

    Doing the basics

--- src/exercises_js/array-practice/01_map_filter.js
+++ src/exercises_js/array-practice/01_map_filter.js
@@ -1,1 +1,34 @@
-console.log("hey");
+// Map
+const numbers = [1, 2, 3, 4, 5];
+
+const doubleNumbers = numbers.map((num) => num * 2);
+
+//Filter
+const ages = [12, 17, 8, 24, 32, 15];
+
+const agesOver18 = ages.filter((age) => age > 18);
+
+//Reduce
+const prices = [10.99, 5.0, 8.5, 3.25];
+
+const numbersR = [1, 2, 3, 4];
+
+const initialValue = -10;
+
+const sumWithInitial = numbersR.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+);
+
+console.log(sumWithInitial);
+const sumNumbersR = numbersR.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
+
+console.log(sumNumbersR);
+
+const sumPrices = prices.reduce(
+  (accumulator, currentPrice) => accumulator + currentPrice
+);
+
+console.log(sumPrices);
