d744bef6850ab45f6b25e5893c852a670a0f415b
Calet Devia <calebtius@gmail.com>
2025-05-20 15:32:42 -0500

    Doing the last exercise?

--- src/exercises_js/array-practice/01_map_filter.js
+++ src/exercises_js/array-practice/01_map_filter.js
@@ -60,15 +60,163 @@ export const PracticeJS = () => {
     { product: "Zapatos", price: 60, quantity: 3 },
   ];
 
-  let last = 0;
-
   const salesByProduct = sales.map((sale) => sale.price * sale.quantity);
 
-  console.log(salesByProduct);
+  // console.log(salesByProduct);
   const totalSales = salesByProduct.reduce(
     (accumulator, currentSale, index, array) => accumulator + currentSale
   );
 
-  console.log(totalSales);
+  // console.log(totalSales);
 
+  /* Contar cuántas personas tienen cada tipo de sangre 
+  
+Devuelve un objeto que cuente cuántas personas hay por cada tipo de sangre. Ejemplo de salida:
+
+  {
+  'O+': 2,
+  'A+': 2,
+  'B-': 1
+}
+  */
+
+  const people = [
+    { name: "Carlos", bloodType: "O+" },
+    { name: "Sofía", bloodType: "A+" },
+    { name: "Juan", bloodType: "O+" },
+    { name: "Ana", bloodType: "B-" },
+    { name: "Luis", bloodType: "A+" },
+  ];
+
+  const peopleOPositive = people.filter(
+    (person) => person.bloodType === "O+"
+  ).length;
+
+  const peopleAPositive = people.filter(
+    (person) => person.bloodType === "A+"
+  ).length;
+
+  const peopleBNegative = people.filter(
+    (person) => person.bloodType === "B-"
+  ).length;
+
+  // const typeBloodData = {
+  //   "O+": peopleOPositive,
+  //   "A+": peopleAPositive,
+  //   "B-": peopleBNegative,
+  // };
+
+  // console.log(typeBloodData);
+  // console.log(peopleOPositive);
+  // console.log(peopleAPositive);
+  // console.log(peopleBNegative);
+
+  let countOPositive = 0;
+  let countAPositive = 0;
+  let countBNegative = 0;
+  const miracle = people.reduce((accumulator, person, index) => {
+    if (person.bloodType === "O+") {
+      // console.log(person.bloodType);
+      countOPositive++;
+    }
+    if (person.bloodType === "A+") {
+      // console.log(person.bloodType);
+      countAPositive++;
+    }
+    if (person.bloodType === "B-") {
+      // console.log(person.bloodType);
+      countBNegative++;
+    }
+
+    return { "O+": countAPositive, "A+": countAPositive, "B-": countBNegative };
+
+    // console.log(index);
+    // console.log(accumulator);
+
+    // console.log(accumulator + person.bloodType);
+  }, 0); // Por descubrir el 0 este me di cuenta que se podia hacer lo que hice ahora, por que en el anterior ejercicio del reduce si te puedes dar cuenta, tube que usar el map porque sin el 0 este en el reduce el currentValue no iniciaba desde el indice 0
+
+  // console.log(countOPositive);
+
+  // console.log(countAPositive);
+
+  // console.log(countBNegative);
+  // console.log(miracle);
+
+  /* 
+  Crear una lista de correos válidos
+
+  Filtra solo los correos válidos (que tengan "@" y terminen en .com).
+  */
+
+  const emails = [
+    "juan@gmail.com",
+    "invalid-email",
+    "ana123@outlook.com",
+    "sofía@hotmail",
+    "luis@gmail.com",
+  ];
+
+  const emailRegex =
+    /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+  const validEmails = emails.filter((email) => {
+    if (emailRegex.test(email)) return email;
+  });
+
+  // console.log(validEmails);
+
+  /*   Devolver un array con los nombres únicos
+
+  Devuelve un array con los nombres únicos, es decir, sin repetir.
+
+ */
+
+  const people1 = [
+    { name: "Ana" },
+    { name: "Luis" },
+    { name: "Ana" },
+    { name: "Carlos" },
+    { name: "Luis" },
+    { name: "María" },
+  ];
+
+  let count = 0;
+  let index1 = 0;
+  let clearRepetitions = [];
+  let nameTimesArray = 0;
+  const noRepeated = people1.filter((person, index) => {
+    // console.log(people1[0]);
+    // console.log(people1[0]);
+
+    console.log("Afuera " + count, index);
+    if (count != index) {
+      console.log(count, index);
+      if (people1[count].name === person.name) {
+        if (true !== index) {
+          console.log(
+            `people1[${count}}].name: ${people1[count].name}\npersona.name, ${index}: ${person.name}`
+          );
+
+          nameTimesArray++;
+          console.log(
+            `Nombre: ${people1[count].name} Vista: ${nameTimesArray}`
+          );
+
+          if (nameTimesArray >= 1) {
+            count++;
+
+            nameTimesArray = 0;
+          }
+        }
+      }
+      return people1.filter((e, i) => i !== count);
+    }
+  }, 0);
+
+  console.log(noRepeated);
+
+  people1.forEach((element) => {
+    // console.log(element);
+  });
 };
