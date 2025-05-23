21978cb23a21ee5ebbcc9e6e17c5553698a0894f
Calet Devia <calebtius@gmail.com>
2025-05-23 10:30:14 -0500

    Finished the deep search in objects exercise

--- src/exercises_js/array-practice/01_map_filter.js
+++ src/exercises_js/array-practice/01_map_filter.js
@@ -1,43 +1,32 @@
 export const PracticeJS = () => {
-  const estudiantes = [
-    { nombre: "Luis", nota: 16 },
-    { nombre: "María", nota: 19 },
-    { nombre: "Pedro", nota: 12 },
-    { nombre: "Lucía", nota: 14 },
-  ];
+  const empresa = {
+    nombre: "TechCorp",
+    empleados: [
+      {
+        nombre: "Carlos",
+        cargo: "Desarrollador",
+        habilidades: ["JS", "React"],
+      },
+      {
+        nombre: "Ana",
+        cargo: "Diseñadora",
+        habilidades: ["Figma", "Photoshop"],
+      },
+    ],
+  };
 
-  // Usa map para crear un array solo con los nombres
-  estudiantes.map((estudiante) => {
-    console.log(estudiante.nombre);
+  // Muestra el nombre del segundo empleado
+
+  console.log(empresa["empleados"][1]["nombre"]);
+
+  // Agrega "TypeScript" a las habilidades de Carlos
+  empresa["empleados"][0]["habilidades"].push("TypeScript");
+  console.log(empresa["empleados"][0]["habilidades"]);
+
+  // Crea un array con todos los nombres de empleados
+  const nombresEmpleados = empresa.empleados.map((empleado) => {
+    return empleado["nombre"];
   });
 
-  console.log(estudiantes);
-
-  // Usa filter para obtener los aprobados (nota >= 14)
-
-  const newEstudiantes = estudiantes.filter((estudiante) => {
-    if (estudiante.nota >= 14) return estudiante;
-  });
-
-  console.log(newEstudiantes);
-
-  // Usa find para buscar a Pedro
-  estudiantes.find((estudiante) => {
-    if (estudiante.nombre === "Pedro") console.log(estudiante);
-  });
-
-  // Usa some para saber si alguien sacó más de 18
-
-  const mayorA18 = estudiantes.some((estudiante) => estudiante > 18);
-
-  console.log(mayorA18);
-  // Usa reduce para sacar el promedio de notas
-
-  const promedio = estudiantes.reduce((acu, estudiante, index, array) => {
-    const nota = estudiante.nota;
-    acu = acu + nota;
-    if (index >= array.length - 1) return acu / 4;
-    return acu;
-  }, 0);
-  console.log(promedio);
+  console.log(nombresEmpleados);
 };
