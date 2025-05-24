24055c4b451da85bed78b6a7aeced9eb5a066222
Calet Devia <calebtius@gmail.com>
2025-05-24 12:48:49 -0500

    Finished the 1.8 exercise

--- src/exercises_js/level-1/exercises.js
+++ src/exercises_js/level-1/exercises.js
@@ -1,30 +1,13 @@
 export const PracticeJS = () => {
-  const students = [
-    { name: "Daniel", grade: 14 },
-    { name: "Isabella", grade: 19 },
-    { name: "Tom", grade: 11 },
-    { name: "Sophia", grade: 17 },
-  ];
+  const user = {
+    name: "Sophie",
+    city: "Paris",
+    age: 32,
+  };
 
-  /*
-1. Create a new array with only the names of the students.
-2. Filter only those who passed (grade >= 14).
-3. Use `reduce()` to calculate the average grade of the class.
-4. Use `some()` to check if any student got more than 18.
- */
+  // Clone the user and add a new property: "isAdmin: true"
+  // Don't modify the original object
+  const cloneUser = { ...user, ["isAdmin"]: true };
 
-  const studentsNames = students.map((student) => student.name);
-  console.log(studentsNames);
-
-  const studentHowPassed = students.filter((student) => student.grade >= 14);
-  console.log(studentHowPassed);
-
-  const averageOfClass =
-    students.reduce((acc, student) => acc + student.grade, 0) / students.length;
-
-  console.log(averageOfClass);
-
-  const greaterThanEighteen = students.some((student) => student.grade > 18);
-
-  console.log(greaterThanEighteen);
+  console.log(cloneUser);
 };
