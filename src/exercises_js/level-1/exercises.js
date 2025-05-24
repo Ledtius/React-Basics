daaeb10ad21e1c0fdf5fe61cd89b9c269e0c9685
Calet Devia <calebtius@gmail.com>
2025-05-24 12:46:20 -0500

    Finished the 1.7 exercise

--- src/exercises_js/level-1/exercises.js
+++ src/exercises_js/level-1/exercises.js
@@ -1,13 +1,30 @@
 export const PracticeJS = () => {
-  const team = [
-    { name: "Emma", role: "Designer" },
-    { name: "Liam", role: "Developer" },
-    { name: "Olivia", role: "Project Manager" },
+  const students = [
+    { name: "Daniel", grade: 14 },
+    { name: "Isabella", grade: 19 },
+    { name: "Tom", grade: 11 },
+    { name: "Sophia", grade: 17 },
   ];
 
-  // Create a new array with only the names
+  /*
+1. Create a new array with only the names of the students.
+2. Filter only those who passed (grade >= 14).
+3. Use `reduce()` to calculate the average grade of the class.
+4. Use `some()` to check if any student got more than 18.
+ */
 
-  const teamNames = team.map((member) => member.name);
+  const studentsNames = students.map((student) => student.name);
+  console.log(studentsNames);
 
-  console.log(teamNames);
+  const studentHowPassed = students.filter((student) => student.grade >= 14);
+  console.log(studentHowPassed);
+
+  const averageOfClass =
+    students.reduce((acc, student) => acc + student.grade, 0) / students.length;
+
+  console.log(averageOfClass);
+
+  const greaterThanEighteen = students.some((student) => student.grade > 18);
+
+  console.log(greaterThanEighteen);
 };
