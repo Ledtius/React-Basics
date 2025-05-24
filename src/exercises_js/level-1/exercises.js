9080239e7239d01ac1336d7735f92cdbeaf03aab
Calet Devia <calebtius@gmail.com>
2025-05-24 12:11:15 -0500

    Finished the 1.1 exercise

---
+++ src/exercises_js/level-1/exercises.js
@@ -0,0 +1,16 @@
+export const PracticeJS = () => {
+  const colors = ["red", "blue", "green", "yellow", "purple"];
+
+  console.log(colors);
+  // 1. Remove the first element
+  colors.shift();
+  console.log(colors);
+
+  // 2. Remove the last element
+  colors.pop();
+  // 3. Print the final array
+  console.log(colors);
+
+
+  
+};
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./task-management/styles/variables.css";
 
 import TaskApp from "./task-management/pages/TaskApp.jsx";
 
-import { PracticeJS } from "./exercises_js/array-practice/01_map_filter.js";
+import { PracticeJS } from "./exercises_js/level-1/exercises.js";
 
 createRoot(document.getElementById("root")).render(
   <>
