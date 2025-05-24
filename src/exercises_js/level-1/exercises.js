0ea9c3fbccc3e3378f304957a20fba823258b436
Calet Devia <calebtius@gmail.com>
2025-05-24 12:19:08 -0500

    Finished the 1.3 exercise

--- src/exercises_js/level-1/exercises.js
+++ src/exercises_js/level-1/exercises.js
@@ -1,9 +1,9 @@
 export const PracticeJS = () => {
-  const numbers = [10, 20, 30, 40, 50, 60];
+  const animals = ["dog", "cat", "panda", "koala", "wolf"];
 
-  // Create a new array that contains only the elements from index 2 to 4 (inclusive)
+  // Create a new array with animals whose name ends with "a"
 
-  const newNumbers = numbers.splice(2, 3);
+  const animalsEndedALetter = animals.filter((animal) => animal.endsWith("a"));
 
-  console.log(newNumbers);
+  console.log(animalsEndedALetter);
 };
