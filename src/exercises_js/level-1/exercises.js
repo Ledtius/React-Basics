608e01fb451431b7432013e3131eba44aa237801
Calet Devia <calebtius@gmail.com>
2025-05-27 08:29:57 -0500

    Exercise 1 of All topics done

--- src/exercises_js/level-1/exercises.js
+++ src/exercises_js/level-1/exercises.js
@@ -1,12 +1,58 @@
 export const PracticeJS = () => {
-  function greet(name = "Guest") {
-    return `Hello, ${name}!`;
+  const users = [
+    { name: "Ana", age: 28, skills: ["JavaScript", "React"] },
+    { name: "Luis", age: 35, skills: ["Java", "Spring", "SQL"] },
+    { name: "Marta", age: 22, skills: ["HTML", "CSS", "JavaScript"] },
+  ];
 
-    // 9. Make this function print "Hello, <name>!" where name is the parameter, or "Guest" if no argument passed.
+  /* 
+1. Create a new array with the names of users older than 25.
+    
+2. Build a **unique skills array** using `Set`.
+    
+3. Use `reduce` to build a summary object like:
+  { totalUsers: 3,
+  averageAge: 28.3,
+  allSkills: [ unique skills ]
   }
+*/
 
-  // 10. Call greet with and without argument.
+  const usersGreater25 = users.filter(({ age }) => age > 25);
 
-  console.log(greet());
-  console.log(greet("Calet"));
+  console.log(usersGreater25);
+
+  let noRepeatSkills = [...new Set(users.map(({ skills }) => skills).flat())];
+
+  console.log(noRepeatSkills);
+
+  //3. The form that the exercise said
+  let countAgeValue = 0;
+  const allUsersInformation = users.reduce((acc, user, index, array) => {
+    const { age } = user;
+
+    acc["totalUsers"] = array.length;
+
+    countAgeValue += age;
+    console.log(countAgeValue);
+
+    if (index === array.length - 1) {
+      acc["averageAge"] = (countAgeValue / array.length).toFixed(1);
+      acc.allSkills = noRepeatSkills;
+    }
+
+    return acc;
+  }, {});
+  // 3. But more easy (Personally)
+  const averageAge = (
+    users.reduce((acc, { age }) => acc + age, 0) / users.length
+  ).toFixed(1);
+
+  const data = {
+    totalUsers: users.length,
+    averageAge,
+    allSkills: noRepeatSkills,
+  };
+
+  console.log(data);
+  console.log(allUsersInformation);
 };
