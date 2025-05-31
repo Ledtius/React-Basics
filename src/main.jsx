b18497ab739e7e5f3a6474603a0b7c48127930c8
Calet Devia <calebtius@gmail.com>
2025-05-31 12:19:32 -0500

    Deleted and reorganize the exercise file

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./task-management/styles/variables.css";
 
 import TaskApp from "./task-management/pages/TaskApp.jsx";
 
-import { PracticeJS } from "./exercises_js/level-1/exercises.js";
+import { PracticeJS } from "./exercises_js/exercises.js";
 
 createRoot(document.getElementById("root")).render(
   <>
