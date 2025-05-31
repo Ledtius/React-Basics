faa6313738a1ee80869ca7ea157c597e8dcce401
Calet Devia <calebtius@gmail.com>
2025-05-31 14:14:52 -0500

    More exercise 5

--- src/exercises_js/exercises.js
+++ src/exercises_js/exercises.js
@@ -1,15 +1,3 @@
 export const PracticeJS = () => {
-  const users = [
-    { name: "Ana", age: 25 },
-    { name: "Luis", age: 30 },
-    { name: "Karen", age: 22 },
-  ];
-
-  for (const { name, age } of users) {
-    console.log(`${name} is ${age} years old`);
-  }
-
-  const onlyNames = users.map(({ name }) => name);
-
-  console.log(onlyNames);
+;
 };
