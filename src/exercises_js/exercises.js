edff1c38785bc11c0ff86e00f80c0e62da79be9c
Calet Devia <calebtius@gmail.com>
2025-05-31 14:18:36 -0500

    More exercise 6

--- src/exercises_js/exercises.js
+++ src/exercises_js/exercises.js
@@ -1,3 +1,8 @@
 export const PracticeJS = () => {
-;
+  const frontendSkills = ["HTML", "CSS", "JavaScript"];
+  const learning = ["JavaScript", "React", "Tailwind"];
+
+  const newFrontendSkills = [...new Set(frontendSkills.concat(learning))];
+
+  console.log(newFrontendSkills);
 };
