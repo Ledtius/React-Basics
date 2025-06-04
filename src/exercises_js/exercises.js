3c3192d4a178083ad9121c208eceec3495e7144f
Calet Devia <calebtius@gmail.com>
2025-06-04 10:03:02 -0500

    Day 1 - Exercise 8

--- src/exercises_js/exercises.js
+++ src/exercises_js/exercises.js
@@ -1,12 +1,30 @@
 export const PracticeJS = () => {
-  const user = {
-    id: 99,
-    username: "Calet23",
-    password: "supersecret",
-    email: "calet@example.com",
+  const tasks = [
+    { id: 1, title: "Study JS", completed: false },
+    { id: 2, title: "Build portfolio", completed: false },
+    { id: 3, title: "Play Valorant", completed: false },
+  ];
+
+  const completeTask = (id) => {
+    console.log(id);
+    const newTasks = [...tasks];
+    return newTasks.map(({ id: idTask, title, completed }) => {
+      console.log(idTask);
+      if (idTask === id) {
+        return { idTask, title, completed: true };
+      }
+      return { idTask, title, completed };
+    });
   };
 
-  const { password, ...email } = user;
-  const newUser = { ...email };
-  console.log(newUser);
+  const [{ id1: id }, { id: id2 }, { id: id3 }] = tasks;
+
+  console.log(completeTask(id3));
+  // 👉 Your task:
+  // 1. Write a function `completeTask(id)` that:
+  //    - takes a task ID
+  //    - returns a new array with that task marked as `completed: true`
+  //    - does NOT mutate the original array
+
+  // 🧠 Tip: use map + spread
 };
