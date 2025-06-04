1e21ad48a5a161fb5caa295697bbe8d8ff7740c7
Calet Devia <calebtius@gmail.com>
2025-06-04 07:46:31 -0500

    Day 1 - Exercise 6

--- src/exercises_js/exercises.js
+++ src/exercises_js/exercises.js
@@ -1,12 +1,19 @@
 export const PracticeJS = () => {
-  const user = {
-    id: 101,
-    name: "Calet",
-    location: "Barrancabermeja",
-    skills: ["HTML", "CSS", "JS"],
+  const users = [
+    { id: 1, name: "Alice", premium: false },
+    { id: 2, name: "Bob", premium: false },
+    { id: 3, name: "Calet", premium: false },
+  ];
+
+  const newUsers = [...users];
+
+  const upgradeUsers = (users) => {
+    return users.map(({ id, name, premium }) => ({
+      id,
+      name,
+      premium: true,
+    }));
   };
 
-  const { id, name, ...info } = user;
-  const newUser = { id, name, ...info, level: "beginner" };
-  console.log(newUser);
+  console.log(upgradeUsers(newUsers));
 };
