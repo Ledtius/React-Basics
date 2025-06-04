a331792b7142b14cf84509d4982b655a1b947b02
Calet Devia <calebtius@gmail.com>
2025-06-04 06:53:18 -0500

    Day 1 - Exercise 1

--- src/exercises_js/exercises.js
+++ src/exercises_js/exercises.js
@@ -1,17 +1,17 @@
 export const PracticeJS = () => {
-  console.log(groupByType(1, "hello", true, 99, "world", false));
-  /* → {
-  number: [1, 99],
-  string: ["hello", "world"],
-  boolean: [true, false]
-} */
+  // Given the following object:
+  const user = {
+    name: "Calet",
+    age: 23,
+    country: "Colombia",
+  };
 
-  function groupByType(...values) {
-    return values.reduce((acc, value) => {
-      const type = typeof value;
-      if (!acc[type]) acc[type] = [];
-      acc[type].push(value);
-      return acc;
-    }, {});
-  }
+  // 👉 Use destructuring to extract each value and log them in a sentence like:
+  // "My name is Calet, I'm 23 years old and I live in Colombia."
+
+  const { name, age, country } = user;
+  
+  console.log(
+    `My name is ${name}, I'm ${age} years old and I live in ${country}`
+  );
 };
