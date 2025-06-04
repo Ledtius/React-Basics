3439f93612280cb83df957b67031849318c19fe9
Calet Devia <calebtius@gmail.com>
2025-06-04 08:06:26 -0500

    Day 1 - Exercise 7

--- src/exercises_js/exercises.js
+++ src/exercises_js/exercises.js
@@ -1,19 +1,12 @@
 export const PracticeJS = () => {
-  const users = [
-    { id: 1, name: "Alice", premium: false },
-    { id: 2, name: "Bob", premium: false },
-    { id: 3, name: "Calet", premium: false },
-  ];
-
-  const newUsers = [...users];
-
-  const upgradeUsers = (users) => {
-    return users.map(({ id, name, premium }) => ({
-      id,
-      name,
-      premium: true,
-    }));
+  const user = {
+    id: 99,
+    username: "Calet23",
+    password: "supersecret",
+    email: "calet@example.com",
   };
 
-  console.log(upgradeUsers(newUsers));
+  const { password, ...email } = user;
+  const newUser = { ...email };
+  console.log(newUser);
 };
