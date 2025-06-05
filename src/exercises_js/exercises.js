546927d24f2d48360eb77b566317add9e2acfd05
Calet Devia <calebtius@gmail.com>
2025-06-05 06:45:06 -0500

    Finish the exercise

--- src/exercises_js/exercises.js
+++ src/exercises_js/exercises.js
@@ -1,30 +1,13 @@
 export const PracticeJS = () => {
-  const tasks = [
-    { id: 1, title: "Study JS", completed: false },
-    { id: 2, title: "Build portfolio", completed: false },
-    { id: 3, title: "Play Valorant", completed: false },
+  const users = [
+    { id: 1, username: "Alice", password: "123", email: "a@a.com" },
+    { id: 2, username: "Bob", password: "456", email: "b@b.com" },
+    { id: 3, username: "Calet", password: "789", email: "c@c.com" },
   ];
 
-  const completeTask = (id) => {
-    console.log(id);
-    const newTasks = [...tasks];
-    return newTasks.map(({ id: idTask, title, completed }) => {
-      console.log(idTask);
-      if (idTask === id) {
-        return { idTask, title, completed: true };
-      }
-      return { idTask, title, completed };
-    });
-  };
+  function sanitizeUsers(users) {
+    return users.map(({ password, ...info }) => info);
+  }
 
-  const [{ id1: id }, { id: id2 }, { id: id3 }] = tasks;
-
-  console.log(completeTask(id3));
-  // 👉 Your task:
-  // 1. Write a function `completeTask(id)` that:
-  //    - takes a task ID
-  //    - returns a new array with that task marked as `completed: true`
-  //    - does NOT mutate the original array
-
-  // 🧠 Tip: use map + spread
+  console.log(sanitizeUsers(users));
 };
