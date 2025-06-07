0557b29991fa403e3bb8e938cc52b1a81e0d9e2f
Calet Devia <calebtius@gmail.com>
2025-06-07 17:14:34 -0500

    Practice the for in and for of 3

--- src/exercises_js/exercises.js
+++ src/exercises_js/exercises.js
@@ -1,28 +1,64 @@
 export const PracticeJS = () => {
   const users = [
-    { name: "Ana", city: "Bogotá", isActive: true },
-    { name: "Luis", city: "Medellín", isActive: false },
-    { name: "María", city: "Bogotá", isActive: true },
-    { name: "Pedro", city: "Cali", isActive: true },
-    { name: "Sofía", city: "Medellín", isActive: true },
+    {
+      name: "Ana",
+      preferences: {
+        theme: "dark",
+        notifications: true,
+      },
+    },
+    {
+      name: "Luis",
+      preferences: {
+        theme: "light",
+        notifications: false,
+      },
+    },
+    {
+      name: "María",
+      preferences: {
+        theme: "dark",
+        notifications: true,
+      },
+    },
+    {
+      name: "Pedro",
+      preferences: null, // este caso debe evitar errores
+    },
   ];
 
-  /* reduce */
+  // Expected output:
+  // { darkTheme: 2, notificationsOn: 2 }
 
-  const reduceVersion = users.reduce((acc, { city, isActive }) => {
-    if (isActive) acc[city] = acc[city] ? acc[city] + 1 : 1;
+  const userInformation = (arrayUsers) => {
+    const info = { darkTheme: 0, notificationsOn: 0 };
+    for (const user of arrayUsers) {
+      if (user.preferences) {
+        const { theme, notifications } = user?.preferences;
 
-    return acc;
-  }, {});
+        const { darkTheme, notificationsOn } = info;
 
-  console.log(reduceVersion);
+        // info.notificationsOn = info.notificationsOn + (notifications ?? 0);
 
-  /* for of  */
-  const forOfVersion = {};
+        /* Es raro que no pueda simplemente escribir notificationesOn + notifications pero si notificationsOn + true o false, es como si a la hora de hacer una comparacion con un operador logico, notifications se transformara su valor original, bueno, en el caso de que usemos el operador Nullish Coalesing */
 
-  for (const { city, isActive } of users) {
-    if (isActive)
-      forOfVersion[city] = forOfVersion[city] ? forOfVersion[city] + 1 : 1;
-  }
-  console.log(forOfVersion);
+        info.notificationsOn = info.notificationsOn + notifications;
+
+        /* Al parecer si se puede, de seguro escribi algo mal en el codigo y le adjudique el error a dicho acontecimiento */
+
+        // info.notificationsOn = notifications
+        //   ? info.notificationsOn + notifications
+        //   : info.notificationsOn;
+
+        if (theme === "dark") info.darkTheme += 1;
+
+        // info.darkTheme = theme === "dark" ? info.darkTheme++ : info.darkTheme;
+      }
+
+      // console.log(user.preferences);
+    }
+    return info;
+  };
+
+  console.log(userInformation(users));
 };
